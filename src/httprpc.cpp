3e6fb31e3895ddca06f9d9fc5a132b0d215eaa37
practicalswift <practicalswift@users.noreply.github.com>
2017-08-15 07:46:56 +0200

    Use MakeUnique<T>(...) instead of std::unique_ptr<T>(new T(...))

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -239,7 +239,7 @@ bool StartHTTPRPC()
     RegisterHTTPHandler("/wallet/", false, HTTPReq_JSONRPC);
 #endif
     assert(EventBase());
-    httpRPCTimerInterface = std::unique_ptr<HTTPRPCTimerInterface>(new HTTPRPCTimerInterface(EventBase()));
+    httpRPCTimerInterface = MakeUnique<HTTPRPCTimerInterface>(EventBase());
     RPCSetTimerInterface(httpRPCTimerInterface.get());
     return true;
 }
