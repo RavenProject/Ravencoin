d56ab5d46da11fc0e3a8b02f4998a6cca816fed6
practicalswift <practicalswift@users.noreply.github.com>
2017-08-15 06:59:08 +0200

    Add MakeUnique (substitute for C++14 std::make_unique)
    From @ryanofsky:s #10973. Thanks!

--- src/util.h
+++ src/util.h
@@ -362,4 +362,11 @@ std::string CopyrightHolders(const std::string &strPrefix);
 
 void SetThreadPriority(int nPriority);
 
+//! Substitute for C++14 std::make_unique.
+template <typename T, typename... Args>
+std::unique_ptr<T> MakeUnique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 #endif // RAVEN_UTIL_H
