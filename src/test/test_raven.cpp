bccd0ce0160b23ae275b285a71beb30b62f7591f
Under <35243703+underdarkskies@users.noreply.github.com>
2018-07-10 20:01:01 -0400

    db: test_raven additional arguments
    This should call CBlockTreeDB with the necessary arguments to compile

--- src/test/test_raven.cpp
+++ src/test/test_raven.cpp
@@ -70,7 +70,7 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
     GetMainSignals().RegisterBackgroundSignalScheduler(scheduler);
 
     mempool.setSanityCheck(1.0);
-    pblocktree = new CBlockTreeDB(1 << 20, true);
+    pblocktree = new CBlockTreeDB(1 << 20, true, false, false, 64, 2 << 20);
     pcoinsdbview = new CCoinsViewDB(1 << 23, true);
     pcoinsTip = new CCoinsViewCache(pcoinsdbview);
     if (!LoadGenesisBlock(chainparams)) {
