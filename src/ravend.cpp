d4667978de10b71942a5d56d18a88019f14725ce
blondfrogs <jeremy.anderson.utah@gmail.com>
2019-09-03 12:43:32 -0600

    Fix testnet params for ravend

--- src/ravend.cpp
+++ src/ravend.cpp
@@ -111,7 +111,7 @@ bool AppInit(int argc, char* argv[])
         }
         // Check for -testnet or -regtest parameter (GetParams() calls are only valid after this clause)
         try {
-            SelectParams(ChainNameFromCommandLine());
+            SelectParams(ChainNameFromCommandLine(), true);
         } catch (const std::exception& e) {
             fprintf(stderr, "Error: %s\n", e.what());
             return false;
