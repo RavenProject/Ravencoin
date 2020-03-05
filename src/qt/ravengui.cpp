56e6e76e4d6631be94ef115d0e38a8bd0c9057b0
blondfrogs <jeremy.anderson.utah@gmail.com>
2020-03-05 11:21:52 -0700

    Add support for disable wallet

--- src/qt/ravengui.cpp
+++ src/qt/ravengui.cpp
@@ -1831,11 +1831,13 @@ void RavenGUI::getPriceInfo()
     networkManager->get(*request);
 }
 
+#ifdef ENABLE_WALLET
 void RavenGUI::mnemonic()
 {
         MnemonicDialog dlg(this);
         dlg.exec();
 }
+#endif
 
 void RavenGUI::getLatestVersion()
 {
