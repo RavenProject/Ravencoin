4ba0b6a22b7eb6075776245b67ea28f28544cb49
blondfrogs <jeremy.anderson.utah@gmail.com>
2020-03-31 14:54:24 -0600

    Fix test

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -251,6 +251,9 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
     result.push_back(Pair("bits", strprintf("%08x", block.nBits)));
     result.push_back(Pair("difficulty", GetDifficulty(blockindex)));
     result.push_back(Pair("chainwork", blockindex->nChainWork.GetHex()));
+    result.push_back(Pair("headerhash", block.GetKAWPOWHeaderHash().GetHex()));
+    result.push_back(Pair("mixhash", block.mix_hash.GetHex()));
+    result.push_back(Pair("nonce64", (uint64_t)block.nNonce64));
 
     if (blockindex->pprev)
         result.push_back(Pair("previousblockhash", blockindex->pprev->GetBlockHash().GetHex()));
