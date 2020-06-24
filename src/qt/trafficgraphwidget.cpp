b0b07e9fee38136547c7973fc6cfe3b99a43c33f
Dominik Opyd <dominik.opyd@gmail.com>
2020-06-24 13:21:38 +0200

    fix build with qt 5.15+

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -7,6 +7,7 @@
 #include "clientmodel.h"
 
 #include <QPainter>
+#include <QPainterPath>
 #include <QColor>
 #include <QTimer>
 
@@ -80,7 +81,7 @@ void TrafficGraphWidget::paintEvent(QPaintEvent *)
 
     const QString units     = tr("KB/s");
     const float yMarginText = 2.0;
-    
+
     // draw lines
     painter.setPen(axisCol);
     painter.drawText(XMARGIN, YMARGIN + h - h * val / fMax-yMarginText, QString("%1 %2").arg(val).arg(units));
