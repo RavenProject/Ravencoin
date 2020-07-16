89b0d15e068cf724b3e9d6839cf1f479c50e6029
Cade Call <cacall@mediciventures.com>
2020-07-15 18:41:40 -0600

    added arm builds back into the mix

--- .github/scripts/05-binary-checks.sh
+++ .github/scripts/05-binary-checks.sh
@@ -30,7 +30,15 @@ elif [[ ${OS} == "linux" || ${OS} == "linux-disable-wallet" ]]; then
     ${GITHUB_WORKSPACE}/src/test/test_raven
 elif [[ ${OS} == "arm32v7" || ${OS} == "arm32v7-disable-wallet" ]]; then
     echo "----------------------------------------"
-    echo "No binary checks available for ${OS}"
+    echo "Checking binary security for ${OS}"
+    echo "----------------------------------------"
+    make -C src check-security
+    echo "----------------------------------------"
+    echo "Running unit tests for ${OS}"
+    echo "----------------------------------------"
+    make check
+    echo "----------------------------------------"
+    echo "Running functional tests for ${OS}"
     echo "----------------------------------------"
 else
     echo "You must pass an OS."
