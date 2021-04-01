237233bccfeff42f223b1e9e3a5738b76d14c577
fdov <fd21@pm.me>
2021-04-01 13:51:20 +0200

    Depends: Update most of the dependencies. Depends: libevent updated to 2.1.11 (Changes copied from bitcoin). Depends: bdb download from https. Depends: Download fontconfig with https. Depends: native_ds_store 1.3.0 (Copied from bitcoin). Depends: Update native_libdmg-hfsplus (Copied from bitcoin). Depends: Update native_mac_alias to 2.1.1 (Copied from bitcoin). Depends: Update zeromq to 4.3.1 (Copied from bitcoin). Depends: boost 1.71.0.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_64_0
-$(package)_download_path=https://dl.bintray.com/boostorg/release/1.64.0/source/
+$(package)_version=1_71_0
+$(package)_download_path=https://dl.bintray.com/boostorg/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
+$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
--- depends/packages/native_mac_alias.mk
+++ depends/packages/native_mac_alias.mk
@@ -1,20 +1,15 @@
 package=native_mac_alias
-$(package)_version=2.0.6
+$(package)_version=2.1.1
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
 $(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=78a3332d9a597eebf09ae652d38ad1e263b28db5c2e6dd725fad357b03b77371
-$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
-$(package)_patches=python3.patch
-
-define $(package)_preprocess_cmds
-    patch -p1 < $($(package)_patch_dir)/python3.patch
-endef
+$(package)_sha256_hash=c0ffceee14f7d04a6eb323fb7b8217dc3f373b346198d2ca42300a8362db7efa
+$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
-    python setup.py build
+    python3 setup.py build
 endef
 
 define $(package)_stage_cmds
     mkdir -p $($(package)_install_libdir) && \
-    python setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
+    python3 setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
 endef
