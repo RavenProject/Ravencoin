8dc2999b3e5f387277701a0dc42767a41a4e38cb
fdov <fd21@pm.me>
2021-04-01 13:51:20 +0200

    Depends: Update Qt 5.12.10, openssl 1.1.1k and most of the dependencies (PR #922)
    Depends: Qt5 5.12.10 and openssl 1.1.1k
    Depends: libevent updated to 2.1.11 (Changes copied from bitcoin).
    Depends: bdb download from https.
    Depends: Download fontconfig with https.
    Depends: native_ds_store 1.3.0 (Copied from bitcoin).
    Depends: Update native_libdmg-hfsplus (Copied from bitcoin).
    Depends: Update native_mac_alias to 2.1.1 (Copied from bitcoin).
    Depends: Update zeromq to 4.3.1 (Copied from bitcoin).
    Depends: boost 1.71.0.
    
           Most of this is copied from bitcoin. (Thanks!)
    
    raven-qt: Add QMacStyle to raven.cpp
    
    Build: Disable shared libraries when building for Windows.
           Mingw32 does not like it. And I can't figure it out.
    
    Build/depends changes:
        - openssl cross compile for osx requires this new version of clang.
        - most build-scripts in depends/ are updated from bitcoin.
        - depends/Makefile tuned to not break the paths.
        - path-changes in openssl.mk to reflect the not broken paths.
        - git build: scripts tuned to work with SDK .tar.gz in /tmp/SDKs.
        - git build: default to python3.
        - python-is-python3 added to depends.

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -156,6 +156,22 @@ endif
 ifneq ($($(1)_ldflags),)
 $(1)_autoconf += LDFLAGS="$$($(1)_ldflags)"
 endif
+
+$(1)_cmake=env CC="$$($(1)_cc)" \
+               CFLAGS="$$($(1)_cppflags) $$($(1)_cflags)" \
+               CXX="$$($(1)_cxx)" \
+               CXXFLAGS="$$($(1)_cppflags) $$($(1)_cxxflags)" \
+               LDFLAGS="$$($(1)_ldflags)" \
+             cmake -DCMAKE_INSTALL_PREFIX:PATH="$$($($(1)_type)_prefix)"
+ifeq ($($(1)_type),build)
+$(1)_cmake += -DCMAKE_INSTALL_RPATH:PATH="$$($($(1)_type)_prefix)/lib"
+else
+ifneq ($(host),$(build))
+$(1)_cmake += -DCMAKE_SYSTEM_NAME=$($(host_os)_cmake_system)
+$(1)_cmake += -DCMAKE_C_COMPILER_TARGET=$(host)
+$(1)_cmake += -DCMAKE_CXX_COMPILER_TARGET=$(host)
+endif
+endif
 endef
 
 define int_add_cmds
