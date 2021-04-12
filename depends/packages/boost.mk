515e814b72b8448cc655cb237bd7c1720f27b58a
fdov <fd21@pm.me>
2021-04-12 20:20:14 +0200

    Depends, boost: update download url #919
        - bintray is closing.
        - update url to jfrog.io.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost
 $(package)_version=1_64_0
-$(package)_download_path=https://dl.bintray.com/boostorg/release/1.64.0/source/
+$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
 
