5777079b6516a8874475628f84d2b6637e26f548
fdov <fd21@pm.me>
2021-04-21 20:11:04 +0200

    Depends: Remove some depends we don't need.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,7 +4,7 @@ native_packages := native_ccache native_b2
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans libxkbcommon
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 qt_arm_linux_packages:=$(qt_x86_64_linux_packages)
 qt_aarch64_linux_packages:=$(qt_x86_64_linux_packages)
