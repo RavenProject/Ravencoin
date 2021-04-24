18372d93b103a92edca4d2fe4371274cc3cc43ce
hans-schmidt <43421934+hans-schmidt@users.noreply.github.com>
2021-04-24 16:37:11 -0500

    Redesigned the mnemonic dialog GUI for wallet creation into 3 less co… (#939)
    * Redesigned the mnemonic dialog GUI for wallet creation into 3 less confusing dialogs
    
    * Add "Makefile.qt.include" changes which the build system wants

--- src/qt/mnemonicdialog.cpp
+++ src/qt/mnemonicdialog.cpp
@@ -1,92 +1,222 @@
+// Copyright (c) 2020 Hans Schmidt
 // Copyright (c) 2017-2018 The Particl Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#define TEST 0
+
 #include <qt/mnemonicdialog.h>
-#include <qt/forms/ui_mnemonicdialog.h>
 
-#include <qt/guiutil.h>
-#include <wallet/wallet.h>
+#include <ui_mnemonicdialog1.h>
+#include <ui_mnemonicdialog2.h>
+#include <ui_mnemonicdialog3.h>
 
-#include <QDebug>
+#if !TEST
+  #include <qt/guiutil.h>
+  #include <wallet/wallet.h>
+#endif
 
 MnemonicDialog::MnemonicDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::MnemonicDialog)
+    QDialog(parent)
 {
-    setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
-    ui->setupUi(this);
+    setWindowTitle(tr("HD Wallet Setup"));
 
-    QObject::connect(ui->btnCancel, &QPushButton::clicked, this, &MnemonicDialog::on_btnCancel_clicked);
-    QObject::connect(ui->btnGenerate, &QPushButton::clicked, this, &MnemonicDialog::on_btnGenerate_clicked);
+    stackedLayout = new QStackedLayout(this);
 
-    ui->tbxMnemonic->installEventFilter(this);
+    MnemonicDialog1 *pgWalletType = new MnemonicDialog1(this);
+    MnemonicDialog2 *pgNewWalletInfo = new MnemonicDialog2(this);
+    MnemonicDialog3 *pgOldWalletInfo = new MnemonicDialog3(this);
 
-    setWindowTitle(QString("HD Wallet Setup"));
-    ui->edtPassword->setPlaceholderText(tr("Enter a passphrase to protect your Recovery Phrase. (optional)"));
-#if QT_VERSION >= 0x050200
-    ui->tbxMnemonic->setPlaceholderText(tr("Enter your BIP39 compliant Recovery Phrase/Mnemonic."));
-#endif
+    connect(pgWalletType,&MnemonicDialog1::updateMainWindowStackWidget,this,&MnemonicDialog::onChangeWindowRequested);
+    connect(pgNewWalletInfo,&MnemonicDialog2::updateMainWindowStackWidget,this,&MnemonicDialog::onChangeWindowRequested);
+    connect(pgOldWalletInfo,&MnemonicDialog3::updateMainWindowStackWidget,this,&MnemonicDialog::onChangeWindowRequested);
+
+    connect(pgNewWalletInfo,&MnemonicDialog2::allCloseRequested,this,&MnemonicDialog::closeMainDialog);
+    connect(pgOldWalletInfo,&MnemonicDialog3::allCloseRequested,this,&MnemonicDialog::closeMainDialog);
+
+    stackedLayout -> addWidget(pgWalletType);
+    stackedLayout -> addWidget(pgNewWalletInfo);
+    stackedLayout -> addWidget(pgOldWalletInfo);
+    stackedLayout -> setCurrentIndex(0);
+
 };
 
-bool MnemonicDialog::eventFilter(QObject *obj, QEvent *ev)
+void MnemonicDialog::onChangeWindowRequested(int ind)  //slot
 {
-    if (obj == ui->tbxMnemonic && ev->type() == QEvent::FocusIn)
-    {
-        // Clear invalid flag on focus
-        ui->lblHelp->clear();
-    }
-    return QWidget::eventFilter(obj, ev);
+    stackedLayout -> setCurrentIndex(ind);
 }
 
-MnemonicDialog::~MnemonicDialog()
+void MnemonicDialog::closeMainDialog()
+{
+    close();
+}
+
+// #############################
+
+MnemonicDialog1::MnemonicDialog1(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::MnemonicDialog1),
+    radioselected("none")
+{
+    ui->setupUi(this);
+};
+
+MnemonicDialog1::~MnemonicDialog1()
 {
     delete ui;
 };
 
-void MnemonicDialog::on_btnCancel_clicked()
+void MnemonicDialog1::on_walletNewRadio_clicked()
 {
-    auto btnRetVal = QMessageBox::question(this, windowTitle(),
-                         tr("If you cancel, the word list will be generated for you. Are you sure you want to continue?"),
-                         QMessageBox::No, QMessageBox::Yes);
-
-    if(btnRetVal == QMessageBox::Yes)
-        close();
+    ui->walletLabel->setText(tr("You are choosing to create a new wallet using new seed words."));
+    radioselected = "new";
 };
 
-void MnemonicDialog::on_btnImport_clicked()
+void MnemonicDialog1::on_walletOldRadio_clicked()
 {
-    std::string words = ui->tbxMnemonic->toPlainText().toStdString();
-    std::string passphrase = ui->edtPassword->text().toStdString();
+    ui->walletLabel->setText(tr("You are choosing to re-create an old wallet using seed words which you know."));
+    radioselected = "old";
+};
 
+void MnemonicDialog1::on_acceptButton_clicked()
+{
+    if (radioselected == "new")
+        Q_EMIT updateMainWindowStackWidget(1);  // "emit" is not supported on older QT revs
+    else if (radioselected == "old")
+        Q_EMIT updateMainWindowStackWidget(2);  // "emit" is not supported on older QT revs
+};
+
+// =========
+
+MnemonicDialog2::MnemonicDialog2(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::MnemonicDialog2)
+{
+    ui->setupUi(this);
+
+};
+
+MnemonicDialog2::~MnemonicDialog2()
+{
+    delete ui;
+};
+
+void MnemonicDialog2::on_backButton_clicked()
+{
+    Q_EMIT updateMainWindowStackWidget(0);  // "emit" is not supported on older QT revs
+};
+
+void MnemonicDialog2::on_acceptButton_clicked()
+{
+    std::string words = MnemonicDialog2::ui->seedwordsText->toPlainText().toStdString();
+    std::string passphrase = MnemonicDialog2::ui->passphraseEdit->text().toStdString();
+
+#if TEST
+    std::string my_words;
+    std::string my_passphrase;
+#endif
     my_words = words;
     my_passphrase = passphrase;
 
+#if TEST
+    // NOTE: default mnemonic passphrase is an empty string
+    if (my_words != "embark lawsuit town sunny forum churn amused gate ensuure smooth valley veteran") {
+#else
     SecureString tmp(my_words.begin(), my_words.end());
 
     // NOTE: default mnemonic passphrase is an empty string
     if (!CMnemonic::Check(tmp)) {
-        ui->lblHelp->setText("Words are not valid, please check the words and try again");
+#endif
+
+        MnemonicDialog2::ui->lblHelp->setText(tr("Words are not valid, please generate new words and try again"));
         my_words.clear();
         my_passphrase.clear();
         return;
     }
 
-    close();
+    Q_EMIT allCloseRequested();
 };
 
-void MnemonicDialog::GenerateWords()
+
+void MnemonicDialog2::on_generateButton_clicked()
 {
+    MnemonicDialog2::ui->lblHelp->clear();
+    GenerateWords();
+};
+
+
+void MnemonicDialog2::GenerateWords()
+{
+#if TEST
+    std::string str_words = "embark lawsuit town sunny forum churn amused gate ensuure smooth valley veteran";
+#else
     SecureString words = CMnemonic::Generate(128);
     std::string str_words = std::string(words.begin(), words.end());
-#if QT_VERSION >= 0x050200
-    ui->tbxMnemonic->setText(QString::fromStdString(str_words));
 #endif
+    MnemonicDialog2::ui->seedwordsText->setPlainText(QString::fromStdString(str_words));
+
 }
 
-void MnemonicDialog::on_btnGenerate_clicked()
+// =========
+
+MnemonicDialog3::MnemonicDialog3(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::MnemonicDialog3)
 {
-   GenerateWords();
+    ui->setupUi(this);
+
+    MnemonicDialog3::ui->seedwordsEdit->installEventFilter(this);
 };
 
+bool MnemonicDialog3::eventFilter(QObject *obj, QEvent *ev)
+{
+    if (obj == MnemonicDialog3::ui->seedwordsEdit && ev->type() == QEvent::FocusIn)
+    {
+        // Clear invalid flag on focus
+        MnemonicDialog3::ui->lblHelp->clear();
+    }
+    return QWidget::eventFilter(obj, ev);
+}
+
+
+MnemonicDialog3::~MnemonicDialog3()
+{
+    delete ui;
+};
+
+void MnemonicDialog3::on_backButton_clicked()
+{
+    Q_EMIT updateMainWindowStackWidget(0);  // "emit" is not supported on older QT revsöU
+};
+
+void MnemonicDialog3::on_acceptButton_clicked()
+{
+    std::string words = MnemonicDialog3::ui->seedwordsEdit->toPlainText().toStdString();
+    std::string passphrase = MnemonicDialog3::ui->passphraseEdit->text().toStdString();
+
+#if TEST
+    std::string my_words;
+    std::string my_passphrase;
+#endif
+    my_words = words;
+    my_passphrase = passphrase;
+
+#if TEST
+    // NOTE: default mnemonic passphrase is an empty string
+    if (my_words != "embark lawsuit town sunny forum churn amused gate ensuure smooth valley veteran") {
+#else
+    SecureString tmp(my_words.begin(), my_words.end());
+
+    // NOTE: default mnemonic passphrase is an empty string
+    if (!CMnemonic::Check(tmp)) {
+#endif
+
+        MnemonicDialog3::ui->lblHelp->setText(tr("Words are not valid, please check the words and try again"));
+        my_words.clear();
+        my_passphrase.clear();
+        return;
+    }
+
+    Q_EMIT allCloseRequested();
+};
 
