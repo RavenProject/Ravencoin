ed77e7cdd5392e4adf14ea10d6ed6a9b2ca5ba46
fdov <fd21@pm.me>
2021-05-16 10:26:24 +0200

    qt, refactor: BanTableModel replace 0 with Qt::NoItemFlags

--- src/qt/peertablemodel.cpp
+++ src/qt/peertablemodel.cpp
@@ -209,8 +209,7 @@ QVariant PeerTableModel::headerData(int section, Qt::Orientation orientation, in
 
 Qt::ItemFlags PeerTableModel::flags(const QModelIndex &index) const
 {
-    if(!index.isValid())
-        return 0;
+    if(!index.isValid()) return Qt::NoItemFlags;
 
     Qt::ItemFlags retval = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
     return retval;
