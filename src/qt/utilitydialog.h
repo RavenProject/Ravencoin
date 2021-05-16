c898342f5a73601a9bbecd8577bc9fcf4742d1fd
fdov <fd21@pm.me>
2021-05-16 10:24:09 +0200

    qt, refactor: utilitydialog shutdown window qflag

--- src/qt/utilitydialog.h
+++ src/qt/utilitydialog.h
@@ -42,7 +42,7 @@ class ShutdownWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit ShutdownWindow(QWidget *parent=0, Qt::WindowFlags f=0);
+    explicit ShutdownWindow(QWidget *parent=nullptr, Qt::WindowFlags f=Qt::Widget);
     static QWidget *showShutdownWindow(RavenGUI *window);
 
 protected:
