389ae55fa09a185e3cf6bf3974cc00f1a9ba3ba2
fdov <fd21@pm.me>
2021-05-19 22:12:15 +0200

    consensus: correct verification of transactions pre p2sh-asset activation
    Handle pre-fork padded P2SH-ish transactions like v4.3.2.1.

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -653,17 +653,19 @@ bool Consensus::CheckTxAssets(const CTransaction& tx, CValidationState& state, c
     int i = 0;
     for (const auto& txout : tx.vout) {
         i++;
-        bool fIsAsset = false;
+
+        // Values are subject to change, by isAssetScript a few lines down.
         int nType = 0;
         int nScriptType = 0;
+        int nStart = 0;
         bool fIsOwner = false;
-        if (txout.scriptPubKey.IsAssetScript(nType, nScriptType, fIsOwner))
-            fIsAsset = true;
 
-        if (fIsAsset && nScriptType == TX_SCRIPTHASH) {
-            if (!AreP2SHAssetsAllowed())
-                return state.DoS(0, false, REJECT_INVALID, "bad-txns-p2sh-assets-not-active");
-        }
+        // False until BIP9 consensus activates P2SH for Assets.
+        bool fP2Active = AreP2SHAssetsAllowed();
+
+        // Returns true if operations on assets are found in the script.
+        // It will also possibly change the values of the arguments, as they are passed by reference.
+        bool fIsAsset = txout.scriptPubKey.IsAssetScript(nType, nScriptType, fIsOwner, nStart, fP2Active);
 
         if (assetCache) {
             if (fIsAsset && !AreAssetsDeployed())
