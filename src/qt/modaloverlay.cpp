5aee68a57c898d77ef5f43ba3250a52ac1268722
fdov <fd21@pm.me>
2021-05-27 23:44:35 +0200

    gui: add version to modal overlay

--- src/qt/modaloverlay.cpp
+++ src/qt/modaloverlay.cpp
@@ -6,12 +6,14 @@
 #include "modaloverlay.h"
 #include "ui_modaloverlay.h"
 
+#include <clientversion.h>
 #include "guiutil.h"
 
 #include "chainparams.h"
 
 #include <QResizeEvent>
 #include <QPropertyAnimation>
+#include <QString>
 
 ModalOverlay::ModalOverlay(QWidget *parent) :
 QWidget(parent),
@@ -30,6 +32,7 @@ userClosed(false)
 
     blockProcessTime.clear();
     setVisible(false);
+    ui->versionLabel->setText(QString::fromStdString(FormatFullVersion()));
 }
 
 ModalOverlay::~ModalOverlay()
