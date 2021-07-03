24c48620928410a5ad47d617c4f96d3dc4c50f24
HenryGSI <henry_eac@hotmail.com>
2021-07-03 18:02:40 +0200

    Add more languages to tests BIP39

--- test/functional/wallet_bip44_multilanguage.py
+++ test/functional/wallet_bip44_multilanguage.py
@@ -11,30 +11,54 @@ from test_framework.test_framework import RavenTestFramework
 from test_framework.util import assert_equal, assert_does_not_contain, assert_contains, assert_raises_rpc_error
 from test_framework.wallet_util import bip39_spanish
 from test_framework.wallet_util import bip39_english
+from test_framework.wallet_util import bip39_french
+from test_framework.wallet_util import bip39_japanese
+from test_framework.wallet_util import bip39_chinese_simplified
+from test_framework.wallet_util import bip39_chinese_traditional
+from test_framework.wallet_util import bip39_korean
+from test_framework.wallet_util import bip39_italian
 
-MNEMONIC_0 = 'puma árbol trago pálido oruga fibra candil jurar humano archivo envase capaz'
+MNEMONIC_0 = 'puma árbol trago pálido oruga fibra candil jurar humano archivo envase capaz' #spanish
 MNEMONIC_PASS_0 = 'test0'
-MNEMONIC_1 = 'untar puerta rito manta audaz copa nácar agregar ligero lucha llenar hígado'
+MNEMONIC_1 = 'sol foto remo casa oreja cita grasa desayuno filial profesor pan salero' #spanish
+MNEMONIC_2 = 'insolite hésiter gazelle ruche anxieux fluvial orange carton linéaire lueur ovation ennuyeux' #french
 MNEMONIC_PASS_2 = 'test2'
-
+MNEMONIC_3 = 'ぞんび せんぞ ずっしり ふこう いほう しゃちょう なにもの かくとく たんおん だんわ にうけ こゆう' #japanese
+MNEMONIC_PASS_3 = 'test3'
+MNEMONIC_4 = '这 飞 炭 亦 偿 锦 昏 燕 炒 徐 军 讯' #chinese simplified
+MNEMONIC_PASS_4 = 'test4'
+MNEMONIC_5 = '這 飛 炭 亦 償 錦 昏 燕 炒 徐 軍 訊' #chinese traditional
+MNEMONIC_PASS_5 = 'test5'
+MNEMONIC_6 = '가슴 법적 잔디 약혼녀 출발 편지 해결 하천 체중 영국 교문 절망' #korean
+MNEMONIC_PASS_6 = 'test6'
+MNEMONIC_7 = 'monviso marcire lavagna snodo appunto inodore radunato ceto olandese orecchino ravveduto fontana' #italian
+MNEMONIC_PASS_7 = 'test7'
 
 class Bip44Test(RavenTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
-        self.num_nodes = 6
+        self.num_nodes = 8
         self.extra_args = [['-bip44=1', '-mnemonic=' + MNEMONIC_0, '-mnemonicpassphrase=' + MNEMONIC_PASS_0], # BIP44 wallet with user-generated 12-words and passphrase
                            ['-bip44=1', '-mnemonic=' + MNEMONIC_1], # BIP44 wallet with user-generated 12-words, but no passphrase
-                           ['-bip44=1', '-mnemonicpassphrase=' + MNEMONIC_PASS_2], # BIP44 wallet with auto-generated 12-words but user-generated passphrase
-                           ['-bip44=1'],    # BIP44 wallet with auto-generated 12-words and no passphrase
-                           ['-bip44=0'],    # BIP44 wallet disabled but supplied with words and passphrase
-                           ['-bip44=0']]    # BIP44 wallet disabled
-
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_2, '-mnemonicpassphrase=' + MNEMONIC_PASS_2], # BIP44 wallet with user-generated 12-words and passphrase
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_3, '-mnemonicpassphrase=' + MNEMONIC_PASS_3], # BIP44 wallet with user-generated 12-words and passphrase
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_4, '-mnemonicpassphrase=' + MNEMONIC_PASS_4], # BIP44 wallet with user-generated 12-words and passphrase
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_5, '-mnemonicpassphrase=' + MNEMONIC_PASS_5], # BIP44 wallet with user-generated 12-words and passphrase
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_6, '-mnemonicpassphrase=' + MNEMONIC_PASS_6], # BIP44 wallet with user-generated 12-words and passphrase
+                           ['-bip44=1', '-mnemonic=' + MNEMONIC_7, '-mnemonicpassphrase=' + MNEMONIC_PASS_7] # BIP44 wallet with user-generated 12-words and passphrase
+                        ]
 
     def run_test(self):
         nodes = self.nodes
 
         # BIP39 list should contain 2048 words
         assert_equal(len(bip39_spanish), 2048)
+        assert_equal(len(bip39_french), 2048)
+        assert_equal(len(bip39_japanese), 2048)
+        assert_equal(len(bip39_chinese_simplified), 2048)
+        assert_equal(len(bip39_chinese_traditional), 2048)
+        assert_equal(len(bip39_korean), 2048)
+        assert_equal(len(bip39_italian), 2048)
 
         # Node 0 has 12 words and a passphrase
         self.log.info("Testing BIP-44 Word-Lists and passphrases")
@@ -47,27 +71,35 @@ class Bip44Test(RavenTestFramework):
         assert_equal(nodes[1].getmywords()['word_list'], MNEMONIC_1) # Word list matches
         assert_does_not_contain(str(nodes[1].getmywords()), 'passphrase') # Passphrase does not exist
 
-        # Node 2 was not created with specific 12 words (using random-auto-generated), and a passphrase
+        # Node 2 has 12 words and a passphrase
         assert_equal(len(nodes[2].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
-        assert(nodes[2].getmywords()['word_list'] != nodes[3].getmywords()['word_list']) # auto-generated word-lists should not match
-        assert_equal(nodes[2].getmywords()['passphrase'], MNEMONIC_PASS_2)
+        assert_equal(nodes[2].getmywords()['word_list'], MNEMONIC_2) # Word list matches
+        assert_equal(nodes[2].getmywords()['passphrase'], MNEMONIC_PASS_2) # Passphrase matches
 
-        # Node 3 was not created with specific 12 words (using random-auto-generated), and no passphrase
+        # Node 3 has 12 words and a passphrase
         assert_equal(len(nodes[3].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
-        assert_does_not_contain(str(nodes[3].getmywords()), 'passphrase') # Passphrase does not exist
+        assert_equal(nodes[3].getmywords()['word_list'], MNEMONIC_3) # Word list matches
+        assert_equal(nodes[3].getmywords()['passphrase'], MNEMONIC_PASS_3) # Passphrase matches
 
-        # Nodes 4 & 5 are not BIP44 and should not have 12-words or passphrase
-        assert_raises_rpc_error(-4, "Wallet doesn't have 12 words.", nodes[4].getmywords)
-        assert_raises_rpc_error(-4, "Wallet doesn't have 12 words.", nodes[5].getmywords)
+        # Node 4 has 12 words and a passphrase
+        assert_equal(len(nodes[4].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
+        assert_equal(nodes[4].getmywords()['word_list'], MNEMONIC_4) # Word list matches
+        assert_equal(nodes[4].getmywords()['passphrase'], MNEMONIC_PASS_4) # Passphrase matches
 
-        # Cannot change from a non-BIP44 to a BIP44 wallet
-        self.log.info("Testing that BIP-44 wallets are intransigent")
-        self.stop_node(4)
-        self.stop_node(5)
-        self.start_node(4, extra_args=['-bip44=1', '-mnemonicpassphrase=test4'])
-        self.start_node(5, extra_args=['-bip44=1', '-mnemonic=' + MNEMONIC_0, '-mnemonicpassphrase=' + MNEMONIC_PASS_0])
-        assert_raises_rpc_error(-4, "Wallet doesn't have 12 words.", nodes[4].getmywords)
-        assert_raises_rpc_error(-4, "Wallet doesn't have 12 words.", nodes[5].getmywords)
+        # Node 5 has 12 words and a passphrase
+        assert_equal(len(nodes[5].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
+        assert_equal(nodes[5].getmywords()['word_list'], MNEMONIC_5) # Word list matches
+        assert_equal(nodes[5].getmywords()['passphrase'], MNEMONIC_PASS_5) # Passphrase matches
+
+        # Node 6 has 12 words and a passphrase
+        assert_equal(len(nodes[6].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
+        assert_equal(nodes[6].getmywords()['word_list'], MNEMONIC_6) # Word list matches
+        assert_equal(nodes[6].getmywords()['passphrase'], MNEMONIC_PASS_6) # Passphrase matches
+
+        # Node 7 has 12 words and a passphrase
+        assert_equal(len(nodes[7].getmywords()['word_list'].split(' ')), 12) # Contains 12 words
+        assert_equal(nodes[7].getmywords()['word_list'], MNEMONIC_7) # Word list matches
+        assert_equal(nodes[7].getmywords()['passphrase'], MNEMONIC_PASS_7) # Passphrase matches
 
         # Try to add a passphrase to an existing bip44 wallet (should not add passphrase)
         self.stop_node(3)
@@ -84,37 +116,39 @@ class Bip44Test(RavenTestFramework):
         self.log.info("Testing that BIP-44 wallets words are valid")
         word_list_0 = MNEMONIC_0.split(' ')
         word_list_1 = MNEMONIC_1.split(' ')
-        word_list_2 = nodes[2].getmywords()['word_list'].split(' ')
-        word_list_3 = word_list_3.split(' ')
+        word_list_2 = MNEMONIC_2.split(' ')
+        word_list_3 = MNEMONIC_3.split(' ')
+        word_list_4 = MNEMONIC_4.split(' ')
+        word_list_5 = MNEMONIC_5.split(' ')
+        word_list_6 = MNEMONIC_6.split(' ')
+        word_list_7 = MNEMONIC_7.split(' ')
         for i in range(0, 12):
             assert_contains(word_list_0[i], bip39_spanish)
             assert_contains(word_list_1[i], bip39_spanish)
-            assert_contains(word_list_2[i], bip39_english)
-            #assert_contains(word_list_3[i], bip39_spanish)
+            assert_contains(word_list_2[i], bip39_french)
+            assert_contains(word_list_3[i], bip39_japanese)
+            assert_contains(word_list_4[i], bip39_chinese_simplified)
+            assert_contains(word_list_5[i], bip39_chinese_traditional)
+            assert_contains(word_list_6[i], bip39_korean)
+            assert_contains(word_list_7[i], bip39_italian)
 
         # None of the words should be text-readable in the log files
         self.log.info("Testing that BIP-44 words aren't text readable")
-        mnemonic_2 = nodes[2].getmywords()['word_list']
-        mnemonic_3 = nodes[3].getmywords()['word_list']
+        mnemonic_1 = nodes[1].getmywords()['word_list']
         self.stop_nodes()
         with open(os.path.join(self.options.tmpdir+"/node0/regtest/", "debug.log"), 'r', encoding='utf8') as f:
             assert_does_not_contain(MNEMONIC_0, f.read())
         with open(os.path.join(self.options.tmpdir+"/node1/regtest/", "debug.log"), 'r', encoding='utf8') as f:
             assert_does_not_contain(MNEMONIC_1, f.read())
         with open(os.path.join(self.options.tmpdir+"/node2/regtest/", "debug.log"), 'r', encoding='utf8') as f:
-            assert_does_not_contain(mnemonic_2, f.read())
+            assert_does_not_contain(MNEMONIC_2, f.read())
         with open(os.path.join(self.options.tmpdir+"/node3/regtest/", "debug.log"), 'r', encoding='utf8') as f:
-            assert_does_not_contain(mnemonic_3, f.read())
+            assert_does_not_contain(MNEMONIC_3, f.read())
 
         # But words are readable in a non-encrypted wallet
-        #with open(os.path.join(self.options.tmpdir+"/node0/regtest/", "wallet.dat"), 'rb') as f:
-        #    assert_contains(MNEMONIC_0, str(f.read()))
-        #with open(os.path.join(self.options.tmpdir+"/node1/regtest/", "wallet.dat"), 'rb') as f:
-        #    assert_contains(MNEMONIC_1, str(f.read()))
-        with open(os.path.join(self.options.tmpdir+"/node2/regtest/", "wallet.dat"), 'rb') as f:
-            assert_contains(mnemonic_2, str(f.read()))
-        with open(os.path.join(self.options.tmpdir+"/node3/regtest/", "wallet.dat"), 'rb') as f:
-            assert_contains(mnemonic_3, str(f.read()))
+      
+        with open(os.path.join(self.options.tmpdir+"/node1/regtest/", "wallet.dat"), 'rb') as f:
+            assert_contains(mnemonic_1, str(f.read()))
 
         # After encryption the words are no longer readable
         self.log.info("Testing that BIP-44 wallet encryption")
@@ -123,6 +157,10 @@ class Bip44Test(RavenTestFramework):
         nodes[1].node_encrypt_wallet("password1")
         nodes[2].node_encrypt_wallet("password2")
         nodes[3].node_encrypt_wallet("password3")
+        nodes[4].node_encrypt_wallet("password4")
+        nodes[5].node_encrypt_wallet("password5")
+        nodes[6].node_encrypt_wallet("password6")
+        nodes[7].node_encrypt_wallet("password7")
         self.stop_nodes()
         # But words are not readable in an encrypted wallet
         with open(os.path.join(self.options.tmpdir+"/node0/regtest/", "wallet.dat"), 'rb') as f:
@@ -130,9 +168,17 @@ class Bip44Test(RavenTestFramework):
         with open(os.path.join(self.options.tmpdir+"/node1/regtest/", "wallet.dat"), 'rb') as f:
             assert_does_not_contain(MNEMONIC_1, str(f.read()))
         with open(os.path.join(self.options.tmpdir+"/node2/regtest/", "wallet.dat"), 'rb') as f:
-            assert_does_not_contain(mnemonic_2, str(f.read()))
+            assert_does_not_contain(MNEMONIC_2, str(f.read()))
         with open(os.path.join(self.options.tmpdir+"/node3/regtest/", "wallet.dat"), 'rb') as f:
-            assert_does_not_contain(mnemonic_3, str(f.read()))
+            assert_does_not_contain(MNEMONIC_3, str(f.read()))
+        with open(os.path.join(self.options.tmpdir+"/node4/regtest/", "wallet.dat"), 'rb') as f:
+            assert_does_not_contain(MNEMONIC_4, str(f.read()))
+        with open(os.path.join(self.options.tmpdir+"/node5/regtest/", "wallet.dat"), 'rb') as f:
+            assert_does_not_contain(MNEMONIC_5, str(f.read()))
+        with open(os.path.join(self.options.tmpdir+"/node6/regtest/", "wallet.dat"), 'rb') as f:
+            assert_does_not_contain(MNEMONIC_6, str(f.read()))
+        with open(os.path.join(self.options.tmpdir+"/node7/regtest/", "wallet.dat"), 'rb') as f:
+            assert_does_not_contain(MNEMONIC_7, str(f.read()))
 
         # But the words are still available using getmywords after entering the passphrase
         self.start_nodes()
