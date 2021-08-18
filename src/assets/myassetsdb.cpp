392b2becb286de0f5fd61ac84a5f2561d2460527
BrockWood <brocks.work@gmail.com>
2021-08-18 13:05:16 -0600

    Updated Copyright Year Using Python Dev Tool (#1080)

--- src/assets/myassetsdb.cpp
+++ src/assets/myassetsdb.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 The Raven Core developers
+// Copyright (c) 2018-2020 The Raven Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "validation.h"
