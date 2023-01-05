af20bbdb24105c8a9b1dcc741aaa9b4cdd89ef71
Hans Schmidt <hans_schm1dt@protonmail.com>
2023-01-05 12:48:39 -0600

    Revert "port veil changes using std::mutex instead of c-style pthread"
    This reverts commit 4c605b696551314c85cfa3d916da717fd9cbda74.

--- src/crypto/ethash/lib/ethash/managed.cpp
+++ src/crypto/ethash/lib/ethash/managed.cpp
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0.
 
 #include "crypto/ethash/lib/ethash/ethash-internal.hpp"
+#include "sync.h"
 
 #include <memory>
-#include <mutex>
 
 #if !defined(__has_cpp_attribute)
 #define __has_cpp_attribute(x) 0
@@ -23,11 +23,12 @@ using namespace ethash;
 
 namespace
 {
-std::mutex shared_context_mutex;
+
+CCriticalSection shared_context_cs;
 std::shared_ptr<epoch_context> shared_context;
 thread_local std::shared_ptr<epoch_context> thread_local_context;
 
-std::mutex shared_context_full_mutex;
+CCriticalSection shared_context_full_cs;
 std::shared_ptr<epoch_context_full> shared_context_full;
 thread_local std::shared_ptr<epoch_context_full> thread_local_context_full;
 
@@ -44,7 +45,7 @@ void update_local_context(int epoch_number)
     thread_local_context.reset();
 
     // Local context invalid, check the shared context.
-    std::lock_guard<std::mutex> lock{shared_context_mutex};
+    LOCK(shared_context_cs);
 
     if (!shared_context || shared_context->epoch_number != epoch_number)
     {
@@ -65,7 +66,7 @@ void update_local_context_full(int epoch_number)
     thread_local_context_full.reset();
 
     // Local context invalid, check the shared context.
-    std::lock_guard<std::mutex> lock{shared_context_full_mutex};
+    LOCK(shared_context_full_cs);
 
     if (!shared_context_full || shared_context_full->epoch_number != epoch_number)
     {
